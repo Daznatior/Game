3d1d2abf583f77a0a61a4f4d7899ff4b18b5d081
bigrosscomcqueenfirefighterextremeyeaboi <rossmccrae1@hotmail.com>
2018-01-26 23:55:12 +0000

    Menu V2
    Changed scaling

--- networking/Assets/update_menu.cs
+++ networking/Assets/update_menu.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class update_menu : MonoBehaviour {
 
+	public Camera maincamera;
+	public GameObject player;
+	GameObject playerClone;
+	Vector3 menu1pos = new Vector3 (403f, 275f, -971f);
+	Vector3 menu2pos = new Vector3 (1711f, 275f, -971f);
+
+	int Y=0;
 	// Use this for initialization
 	void Start () {
-		
+		var scene = SceneManager.GetSceneByName("menu");
+		SceneManager.SetActiveScene(scene);
+		playerClone = (GameObject)Instantiate (player, new Vector3(1727f,291f,-789f), transform.rotation);
+		playerClone.transform.localScale = new Vector3(20f,20f,20f);
 	}
 	
 	// Update is called once per frame
@@ -19,7 +29,25 @@ public class update_menu : MonoBehaviour {
 			btncolors.normalColor = new Color(0,255,0);
 
 		} 
-
+		playerClone.GetComponent<MeshRenderer> ().material.color = new Color (GameObject.Find ("slider_r").GetComponent<Slider> ().value,
+			GameObject.Find ("slider_g").GetComponent<Slider> ().value,
+			GameObject.Find ("slider_b").GetComponent<Slider> ().value);
+		playerClone.transform.Rotate (Vector3.up * (Time.deltaTime*10f));
+		Y++;
 		GameObject.Find ("Button_Join").GetComponent<Button> ().colors = btncolors;
 	}
+
+
+	public void tomainmenu()
+	{
+		GameObject.Find ("panel_main").GetComponent<CanvasGroup> ().alpha = 1f;
+		GameObject.Find ("panel_blockedit").GetComponent<CanvasGroup> ().alpha = 0f;
+	}
+
+	public void toblockedit()
+	{
+		GameObject.Find ("panel_main").GetComponent<CanvasGroup> ().alpha = 1f;
+		GameObject.Find ("panel_blockedit").GetComponent<CanvasGroup> ().alpha = 0f;
+	}
 }
+
