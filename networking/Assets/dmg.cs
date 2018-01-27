15225ad6efa483a2f3777e46c6f7601678d2a5f4
Daznatior <Daznatior@live.co.uk>
2018-01-27 06:56:23 +0000

    update1
    sdfdf

---
+++ networking/Assets/dmg.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class dmg : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall1")
+        {
+            Debug.Log("hit ye ya fucking wall boii");
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+
+            GameObject.Find("Player").GetComponent<PlayerController>().hp = GameObject.Find("Player").GetComponent<PlayerController>().hp - 5;
+
+            
+            Debug.Log(GameObject.Find("Player").GetComponent<PlayerController>().hp);
+        }
+
+    }
+}
